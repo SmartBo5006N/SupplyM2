8756c26a7187dfa00d0e1b93ce404e1df613ca87
SmartBo5006N <74193484+SmartBo5006N@users.noreply.github.com>
2020-11-30 12:37:46 +0530

    Add files via upload

---
+++ obstacle.js
@@ -0,0 +1,21 @@
+class Obstacle {
+    constructor(x,y,width,height){
+        var options = {
+             restitution :0, 
+             friction :0.3, 
+             density :1,
+             isStatic: true }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width; this.height = height;
+         World.add(world, this.body); 
+        } 
+        
+        display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("red"); 
+        rect(this.body.position.x,this.body.position.y,this.width,this.height); 
+        }
+     }
